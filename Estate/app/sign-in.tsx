30667a5b697573b8107b0021ef47023cba42b044
MohammadAB828 <mohammad.abedi5513@gmail.com>
2025-03-07 17:43:37 +0330

    build(deps): update sign-in.tsx, appwrite.ts, package-lock.json and package.json

--- Estate/app/sign-in.tsx
+++ Estate/app/sign-in.tsx
@@ -4,8 +4,11 @@ import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import images from '@/constants/images';
 import icons from '@/constants/icons';
 import { login } from '@/lib/appwrite';
+import { userGlobalContext } from '@/lib/global-provider';
 
 const SignIn = () => {
+
+  const { refetch , loading , isLoggedIn} = userGlobalContext();
 
   const handelLogin = async () => {
     const result = await login();
@@ -13,61 +16,17 @@ const SignIn = () => {
     if(result){
       console.log('Login Success');
     } else {
-      Alert.alert('Error' , 'Failed to login');
+      Alert.alert('Error' , 'Failed to loginnn');
     }
   };
 
 
   return (
-    // <SafeAreaProvider className='h-full w-full'>
-    // <SafeAreaView className=' h-full flex-1' >
-    //   <ScrollView
-    //   className="bg-white h-screen" contentContainerClassName=' flex-grow'>
-    //     <Image source={images.onboarding} className="w-full h-4/6" resizeMode='contain' />
-
-    //     <View className="px-10">
-    //       <Text className="text-base text-center uppercase font-rubik-medium text-black-100">Welcome to Restate</Text>
-
-    //       <Text className="text-3xl text-center font-rubik-bold text-black-300 mt-2">Let's Get You Closer to {"\n"}
-    //         <Text className="text-primary-300">Your Ideal Home</Text>
-    //       </Text>
-
-    //       <Text className="text-lg font-rubik-semibold text-black-100 text-center mt-12">
-    //         Login to ReState with Googlee
-    //       </Text>
-
-    //       <TouchableOpacity onPress={handelLogin} className="bg-white shadow-md shadow-zinc-300 rounded-full w-full">
-    //         <View className="flex flex-row items-center justify-center">
-    //           <Image 
-    //             source={icons.google}
-    //             className="w-5 h-5"
-    //             resizeMode="contain"
-    //           />
-    //           <Text className="text-lg font-rubik-medium text-black-300 text-center ml-2">
-    //             Continue with Google
-    //           </Text>
-    //         </View>
-    //       </TouchableOpacity>
-
-    //     <Text className='text-3xl'>
-    //       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-    //       eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-    //       minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-    //       aliquip ex ea commodo consequat. Duis aute irure dolor in
-    //       reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-    //       pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-    //       culpa qui officia deserunt mollit anim id est laborum.
-    //     </Text>
-         
-    //     </View>
-    //   </ScrollView>
-    // </SafeAreaView>
-    // </SafeAreaProvider>
-
-
-        <SafeAreaView className='flex-1 ' edges={['top']}>
-          <ScrollView className='bg-white pt-8'>
-          <Image source={images.onboarding} className="w-full h-2/6" resizeMode='contain' />
+   
+        <SafeAreaView className='flex-1 h-full' edges={['top']}>
+          <ScrollView className='bg-white pt-8'
+          contentContainerClassName='h-full'>
+          <Image source={images.onboarding} className="w-full h-4/6" resizeMode='contain' />
 
           <View className="px-10">
            <Text className="text-base text-center uppercase font-rubik text-black-100">Welcome to Restate</Text>
@@ -76,34 +35,24 @@ const SignIn = () => {
              <Text className="text-primary-300">Your Ideal Home</Text>
            </Text>
 
-            <Text className="text-lg font-rubik-semibold text-black-100 text-center mt-12">
-             Login to ReState with Googlee
+            <Text className="text-lg font-rubik-semibold text-black-100 text-center my-6">
+             Login to ReState with Google
            </Text>
 
-           <TouchableOpacity onPress={handelLogin} className="bg-white shadow-md shadow-zinc-300 rounded-full w-full">
+           <TouchableOpacity onPress={handelLogin} className="bg-white shadow-lg shadow-zinc-500 rounded-full w-full py-3">
              <View className="flex flex-row items-center justify-center">
                <Image 
                  source={icons.google}
-                 className="w-5 h-5"
+                 className="w-5 h-7"
                  resizeMode="contain"
                />
-               <Text className="text-lg font-rubik-medium text-black-300 text-center ml-2">
-                 Continue with Google
+               <Text className="text-lg font-rubik-medium text-black-300 ml-3 text-center">
+                 Sign Up with Google
                </Text>
              </View>
            </TouchableOpacity>
-
          </View>
 
-            <Text className='text-6xl'>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-              minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-              aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-              pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-              culpa qui officia deserunt mollit anim id est laborum.
-            </Text>
           </ScrollView>
         </SafeAreaView>
   )
--- Estate/lib/appwrite.ts
+++ Estate/lib/appwrite.ts
@@ -20,48 +20,89 @@ export const avatar = new Avatars(client);
 export const account = new Account(client);
 
 
-export async function login() {
-    try{
-        const redirectUri = Linking.createURL('/');
-
-        const response = await account.createOAuth2Token(OAuthProvider.Google,redirectUri);
-
-        if(!response){
-            throw new Error('Failed to Login');
-        }
-
-        const browserResult = await openAuthSessionAsync(
-            response.toString(),
-            redirectUri
-        )
-
-        if(browserResult.type != 'success'){
-            throw new Error('Failed to Login')
-        }
-
-        const url = new URL(browserResult.url);
-
-        const secret = url.searchParams.get('secret')?.toString();
-        const userId = url.searchParams.get('userId')?.toString();
-
-        if(!secret || !userId){
-            throw new Error('Failed to Login');
-        }
-
-        const session = await account.createSession(userId, secret);
-
-        if(!session){
-            throw new Error('Failed to create a session');
-        }
-
-        return true;
-
-    } catch (error){
-        console.error(error);
-        return false;
-    }
+// export async function login() {
     
-}
+//     try{
+//         const redirectUri = Linking.createURL('/');
+
+//          const response = await account.createOAuth2Token(OAuthProvider.Google, redirectUri);
+
+//         if(!response){
+//             throw new Error('Failed to Login');
+//         }
+
+//         const browserResult = await openAuthSessionAsync(
+//             response.toString(),
+//             redirectUri
+//         )
+
+//         console.log('====================================');
+//         console.log(browserResult);
+//         console.log('====================================');
+        
+        
+
+//         if(browserResult.type !== "success"){
+//             throw new Error('Failed to Login')
+//         }
+       
+//         const url = new URL(browserResult.url);
+
+//         const secret = url.searchParams.get("secret")?.toString();
+//         const userId = url.searchParams.get("userId")?.toString();
+
+//         if(!secret || !userId){
+//             throw new Error('Failed to Login');
+//         }
+
+//         const session = await account.createSession(userId, secret);
+
+//         if(!session){
+//             throw new Error('Failed to create a session');
+//         }
+
+//         return true;
+
+//     } catch (error){
+//         console.error(error);
+//         return false;
+//     }
+    
+// }
+
+export async function login() {
+    try {
+      const redirectUri = Linking.createURL("/");
+  
+      const response = await account.createOAuth2Token(
+        OAuthProvider.Google,
+        redirectUri
+      );
+
+
+      if (!response) throw new Error("Create OAuth2 token failed");
+  
+      const browserResult = await openAuthSessionAsync(
+        response.toString(),
+        redirectUri
+      );
+      if (browserResult.type !== "success")
+        throw new Error("Create OAuth2 token failed");
+  
+      const url = new URL(browserResult.url);
+      const secret = url.searchParams.get("secret")?.toString();
+      const userId = url.searchParams.get("userId")?.toString();
+      if (!secret || !userId) throw new Error("Create OAuth2 token failed");
+  
+      const session = await account.createSession(userId, secret);
+      if (!session) throw new Error("Failed to create session");
+  
+      return true;
+    } catch (error) {
+      console.error(error);
+      return false;
+    }
+  }
 
 export async function logout() {
     try{
